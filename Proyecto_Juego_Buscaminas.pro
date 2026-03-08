ebfc0f38e9258094f8e20e209eb059572cad91c6
NajmahZablah <najmahzablah@gmail.com>
2026-03-08 15:47:53 -0600

    Libreia SFML

--- Proyecto_Juego_Buscaminas.pro
+++ Proyecto_Juego_Buscaminas.pro
@@ -5,3 +5,19 @@ CONFIG -= qt
 
 SOURCES += \
         main.cpp
+
+SFML_DIR = C:/SFML-2.6.2
+
+INCLUDEPATH += $$SFML_DIR/include
+
+CONFIG(debug, debug|release) {
+    LIBS += -L$$SFML_DIR/lib \
+            -lsfml-graphics-d \
+            -lsfml-window-d \
+            -lsfml-system-d
+} else {
+    LIBS += -L$$SFML_DIR/lib \
+            -lsfml-graphics \
+            -lsfml-window \
+            -lsfml-system
+}
--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-using namespace std;
-
-int main()
-{
-    cout << "Hello World!" << endl;
+int main() {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Run");
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event))
+            if (event.type == sf::Event::Closed)
+                window.close();
+        window.clear(sf::Color::Blue);
+        window.display();
+    }
     return 0;
 }
